a8d22fe13785a32b0272c46fd76ccf82378c097f
SPBpiraT <intake@list.ru>
2025-02-05 22:44:06 +0300

    Removed some surplus usings

--- MouseCoordinates.Application/Common/Mapping/IMapWith.cs
+++ MouseCoordinates.Application/Common/Mapping/IMapWith.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
 
 namespace MouseCoordinates.Application.Common.Mapping
 {
-    public interface IMapWith
+    public interface IMapWith<T>
     {
-
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
     }
 }
--- MouseCoordinates.Persistence/CoordinatesDbContext.cs
+++ MouseCoordinates.Persistence/CoordinatesDbContext.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MouseCoordinates.Application.Interfaces;
 using MouseCoordinates.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MouseCoordinates.Persistence.EntityTypeConfigurations;
 
 namespace MouseCoordinates.Persistence
 {
     public class CoordinatesDbContext : DbContext, ICoordinatesDbContext
     {
         public DbSet<Coordinates> Coords { get; set; }
+
+        public CoordinatesDbContext(DbContextOptions<CoordinatesDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new CoordinatesConfiguration());
+            base.OnModelCreating(builder);
+        }
     }
 }

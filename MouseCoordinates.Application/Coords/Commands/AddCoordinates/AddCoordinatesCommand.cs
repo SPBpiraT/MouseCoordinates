5ba490b16c340ef484980aa911736a1a0127a2da
SPBpiraT <intake@list.ru>
2025-02-05 23:39:34 +0300

    Implemented CQRS

---
+++ MouseCoordinates.Application/Coords/Commands/AddCoordinates/AddCoordinatesCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace MouseCoordinates.Application.Coords.Commands.AddCoordinates
+{
+    public class AddCoordinatesCommand : IRequest<Guid>
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ MouseCoordinates.Application/Coords/Commands/AddCoordinates/AddCoordinatesCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using MouseCoordinates.Application.Interfaces;
+using MouseCoordinates.Domain;
+
+namespace MouseCoordinates.Application.Coords.Commands.AddCoordinates
+{
+    public class AddCoordinatesCommandHandler
+        : IRequestHandler<AddCoordinatesCommand, Guid>
+    {
+        private readonly ICoordinatesDbContext _dbContext;
+        public AddCoordinatesCommandHandler(ICoordinatesDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Guid> Handle(AddCoordinatesCommand request,
+            CancellationToken cancellationToken)
+        {
+            var coordinates = new Coordinates
+            {
+                Id = Guid.NewGuid(),
+                X = request.X,
+                Y = request.Y,
+                CreationDate = DateTime.Now
+            };
+
+            await _dbContext.Coords.AddAsync(coordinates, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return coordinates.Id;
+        }
+    }
+}

7111e5b56910b636d5d50ca9a7ad8f6a309e7f0d
SPBpiraT <intake@list.ru>
2025-02-05 22:06:52 +0300

    Added DbContext and entity config

---
+++ MouseCoordinates.Domain/Coordinates.cs
@@ -0,0 +1,10 @@
+﻿namespace MouseCoordinates.Domain
+{
+    public class Coordinates
+    {
+        public Guid Id { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}

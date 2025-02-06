2ede9a2c81078f8b4752d1ad67ef468902464216
SPBpiraT <intake@list.ru>
2025-02-06 16:26:48 +0300

    Added controllers and dto model to webapi project

---
+++ MouseCoordinates.WebApi/Controllers/MouseTrackController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MouseCoordinates.Application.Coords.Commands.AddCoordinates;
+using MouseCoordinates.WebApi.Models;
+
+namespace MouseCoordinates.WebApi.Controllers
+{
+    public class MouseTrackController : BaseController
+    {
+        private readonly IMapper _mapper;
+
+        public MouseTrackController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Add([FromBody] AddCoordinatesDto createMessageDto)
+        {
+            var command = _mapper.Map<AddCoordinatesCommand>(createMessageDto);
+            var messageId = await Mediator.Send(command);
+            return Ok(messageId);
+        }
+    }
+}
---
+++ MouseCoordinates.WebApi/Models/AddCoordinatesDto.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MouseCoordinates.Application.Common.Mapping;
+using MouseCoordinates.Application.Coords.Commands.AddCoordinates;
+
+namespace MouseCoordinates.WebApi.Models
+{
+    public class AddCoordinatesDto : IMapWith<AddCoordinatesCommand>
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<AddCoordinatesDto, AddCoordinatesCommand>()
+                .ForMember(coordCommand => coordCommand.X,
+                    opt => opt.MapFrom(coordDto => coordDto.X))
+                .ForMember(coordCommand => coordCommand.Y,
+                    opt => opt.MapFrom(coordDto => coordDto.Y));
+        }
+    }
+}

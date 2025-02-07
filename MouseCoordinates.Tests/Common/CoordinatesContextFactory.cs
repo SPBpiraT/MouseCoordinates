3a82646217e1470cfb5d172c2d65d65724d71012
SPBpiraT <intake@list.ru>
2025-02-07 17:06:26 +0300

    Added command handler test.

---
+++ MouseCoordinates.Tests/Common/CoordinatesContextFactory.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MouseCoordinates.Persistence;
+using MouseCoordinates.Domain;
+using Newtonsoft.Json;
+
+namespace MouseCoordinates.Tests.Common
+{
+    public static class CoordinatesContextFactory
+    {
+        public static CoordinatesDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<CoordinatesDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new CoordinatesDbContext(options);
+
+            context.Database.EnsureCreated();
+
+            context.Coords.AddRange(
+                new Coordinates
+                {
+                    Id = Guid.NewGuid(),
+                    CoordsJson = JsonConvert.SerializeObject(TestDataProvider.GenerateCoordinatesTestData(10))
+                },
+                new Coordinates
+                {
+                    Id = Guid.NewGuid(),
+                    CoordsJson = JsonConvert.SerializeObject(TestDataProvider.GenerateCoordinatesTestData(10))
+                }
+            );
+
+            context.SaveChanges();
+            return context;
+        }
+
+        public static void Destroy(CoordinatesDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
---
+++ MouseCoordinates.Tests/Common/TestCommandBase.cs
@@ -0,0 +1,19 @@
+﻿using MouseCoordinates.Persistence;
+
+namespace MouseCoordinates.Tests.Common
+{
+    public abstract class TestCommandBase : IDisposable
+    {
+        protected readonly CoordinatesDbContext Context;
+
+        public TestCommandBase()
+        {
+            Context = CoordinatesContextFactory.Create();
+        }
+
+        public void Dispose()
+        {
+            CoordinatesContextFactory.Destroy(Context);
+        }
+    }
+}
